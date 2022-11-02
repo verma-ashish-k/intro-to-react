a96de8d01a3d0028e7b522f467bc1da41e7678e9
Ashish Verma <113393491+the3rdeyecan@users.noreply.github.com>
2022-11-02 11:27:22 -0600

    added a counter with buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  function increaseCount(amount) {
+    setCount(count + amount);
+  }
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <header className='App-header'>
+        {count}
+        <button onClick={() => increaseCount(1)}>Increase Count</button>
+        <button onClick={() => increaseCount(-1)}>Decrease Count</button>
+      </header>
     </div>
   );
 }
